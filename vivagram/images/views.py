86579e35d28a6e71e839e189b6297b2d47fd48be
VIVAFROG <vivafrog@gmail.com>
2018-11-25 15:59:31 +0900

    1-39 user feed

--- vivagram/images/views.py
+++ vivagram/images/views.py
@@ -7,29 +7,18 @@ from . import models, serializers
 # Create your views here.
 
 
-class ListAllImages(APIView):
+class Feed(APIView):
 
     def get(self, request, format=None):
-        # format None means JSON
-        all_images = models.Image.objects.all()
-        serializer = serializers.ImageSerializer(all_images, many=True)
 
-        return Response(data=serializer.data)
+        user = request.user
+        following_users = user.following.all()
+        image_list = []
+        for following_user in following_users:
+            user_images = following_user.images.all()[:2]
+            for image in user_images:
+                image_list.apped(image)
+        sorted_list = sorted(image_list, key=lambda image: image.created_at, reverse=True)
 
-
-class ListAllComments(APIView):
-
-    def get(self, request, format=None):
-        all_comments = models.Comment.objects.all()
-        serializer = serializers.CommentSerializer(all_comments, many=True)
-
-        return Response(data=serializer.data)
-
-
-class ListAllLikes(APIView):
-
-    def get(self, request, format=None):
-        all_likes = models.Like.objects.all()
-        serializer = serializers.LikeSerializer(all_likes, many=True)
-
-        return Response(data=serializer.data)
+        serializer = serializers.ImageSerializer(sorted_list, many=True)
+        return Response(serializer.data)

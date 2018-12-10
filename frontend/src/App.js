5efbca942ed3e9ba7b49c927da9d5e5098f75dac
VIVAFROG <vivafrog@gmail.com>
2018-12-10 22:33:06 +0900

    2-8 (react v2)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import style from './App.module.scss';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <div className={style.App}>
+        <header className={style["App-header"]}>
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.

de8a1d3aec3aafa8979ce79c3df174d102bd60f8
VIVAFROG <vivafrog@gmail.com>
2018-12-11 23:29:59 +0900

    3-6

--- frontend/src/redux/configureStore.js
+++ frontend/src/redux/configureStore.js
@@ -1,13 +1,21 @@
-import {createStore, combineReducers, applyMiddleware} from "redux";
-import thunk from 'redux-thunk';
-import users from 'redux/modules/users';
+import { createStore, combineReducers, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import users from "redux/modules/users";
+
+const env = process.env.NODE_ENV;
 
 const middlewares = [thunk];
 
-const reducer = combineReducers({
-    users,
-})
+if (env === "development") {
+  const { logger } = require("redux-logger");
+  middlewares.push(logger);
+}
 
-let store = initialState => createStore(reducer, applyMiddleware(...middlewares));
+const reducer = combineReducers({
+  users
+});
+
+let store = initialState =>
+  createStore(reducer, applyMiddleware(...middlewares));
 
 export default store();

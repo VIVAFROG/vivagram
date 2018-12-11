62a0fb97a15de9094676615a60b72fb9a777d9b1
VIVAFROG <vivafrog@gmail.com>
2018-12-11 22:45:10 +0900

    3-2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,10 +7,6 @@ class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-        <header className={styles.appHeader}>
-          <h1 className={styles.appTitle}>Welcome to React</h1>
-        </header>
-        <p className={styles.appIntro}>To get started, whatever</p>
       </div>
     );
   }

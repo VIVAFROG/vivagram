35a1891b36d6e8fbcfbb609dec7b7aed9acd4ab5
VIVAFROG <vivafrog@gmail.com>
2018-12-12 22:17:41 +0900

    3-11 (I don't know)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "redux/configureStore";
-import "index.css";
+import I18n from "redux-i18n";
+import store, { history } from "redux/configureStore";
 import App from "App";
-
+import { translations } from "translations";
+import { ConnectedRouter } from "connected-react-router";
 
 ReactDOM.render(
   <Provider store={store}>
-    <App />
+    <ConnectedRouter history={history}>
+      <I18n translations={translations} initialLang="en" fallbackLang="en">
+        <App />
+      </I18n>
+    </ConnectedRouter>
   </Provider>,
   document.getElementById("root")
 );

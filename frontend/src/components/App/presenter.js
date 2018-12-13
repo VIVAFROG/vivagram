c60dab07dfbe65144bf74b256eb5be4b4dc6df2a
VIVAFROG <vivafrog@gmail.com>
2018-12-13 23:00:17 +0900

    3-23 making router

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,21 +1,27 @@
-import React, { Component } from "react";
+import React from "react";
 import { Route, Switch } from "react-router-dom";
 import styles from "./styles.scss";
 import Footer from "components/Footer";
 // fetch('/notifications/')
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.App}>
-        <Switch>
-          <Route exact path="/" render={() => "Hello!"} />
-          <Route path="/login" render={() => "Login!"} />
-        </Switch>
-        <Footer />
-      </div>
-    );
-  }
-}
+const App = props => [
+  //Nav,
+  props.isLoggedIn ? <PrivateRoutes key={2} /> : <PublicRoutes key={2} />,
+  <Footer key={3} />
+];
+
+const PrivateRoutes = props => (
+  <Switch>
+    <Route exact path ="/" render={() =>"feed"} />
+    <Route exact path ="/explore" render={() =>"explore"} />
+  </Switch>
+);
+
+const PublicRoutes = props => (
+  <Switch>
+    <Route exact path ="/" render={() =>"login"} />
+    <Route exact path ="/forgot" render={() =>"password"} />
+  </Switch>
+);
 
 export default App;

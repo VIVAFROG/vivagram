e9b486443b4d82b9e2c0244e98574829dccd0dc5
VIVAFROG <vivafrog@gmail.com>
2018-12-13 22:50:59 +0900

    pratice route

--- frontend/src/components/App/presenter.js
+++ frontend/src/components/App/presenter.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import styles from './styles.scss';
-import Footer from "components/Footer"
+import React, { Component } from "react";
+import { Route, Switch } from "react-router-dom";
+import styles from "./styles.scss";
+import Footer from "components/Footer";
 // fetch('/notifications/')
 
 class App extends Component {
   render() {
     return (
       <div className={styles.App}>
-      <Footer />
+        <Switch>
+          <Route exact path="/" render={() => "Hello!"} />
+          <Route path="/login" render={() => "Login!"} />
+        </Switch>
+        <Footer />
       </div>
     );
   }
